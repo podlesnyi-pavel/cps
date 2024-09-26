19698a27a5e60857a40c75b548fcbc988dc6087e
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2024-09-26 19:25:07 +0300

    add brands icons, swiper.js(js, css), rename index.htm on current.html, brands section name = index.html, add main.js, add modifs action-button, styles brans section, change section-title

---
+++ components/swiper/swiper.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  slidesPerView: 'auto',
+  spaceBetween: 16,
+  autoplay: {
+    delay: 3000,
+  },
+  breakpoints: {
+    768: {
+      autoplay: false,
+      pagination: false,
+      simulateTouch: false,
+      spaceBetween: 0,
+    },
+  },
+});
---
+++ main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const swiperWrapper = document.querySelector('.swiper__wrapper');
+const brandsButton = document.querySelector(
+  '.brands-section__show-more-button'
+);
+const textBrandsButton = brandsButton.querySelector('.show-more-button__text');
+
+brandsButton.addEventListener('click', function () {
+  swiperWrapper.classList.toggle('swiper__wrapper--show-all');
+
+  if (swiperWrapper.classList.contains('swiper__wrapper--show-all')) {
+    textBrandsButton.textContent = 'Скрыть';
+    brandsButton.classList.add(
+      'show-more-button--active',
+      'brands-section__show-more-button--active'
+    );
+  } else {
+    textBrandsButton.textContent = 'Показать все';
+    brandsButton.classList.remove(
+      'show-more-button--active',
+      'brands-section__show-more-button--active'
+    );
+  }
+});
